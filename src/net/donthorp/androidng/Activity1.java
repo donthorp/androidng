f6f52ba0bfda3a7d45de4fb441651385c189f036
donthorp <don@donthorp.net>
2010-08-03 20:32:24 -0500

    Renabled support for Activities

--- src/net/donthorp/androidng/Activity1.java
+++ src/net/donthorp/androidng/Activity1.java
@@ -3,14 +3,16 @@
  */
 package net.donthorp.androidng;
 
+import ti.modules.titanium.android.TiBaseActivity;
+
 
 /**
  * @author dthorp
  *
  */
-public class Activity1 /*extends TiBaseActivity*/ 
+public class Activity1 extends TiBaseActivity 
 {
 	public Activity1() {
-		//super("app://app-activity1.js");
+		super("app://app-activity1.js");
 	}
 }
