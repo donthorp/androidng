eef7764829e2d49ca280aba815d7bf69b572c00d
donthorp <don@donthorp.net>
2010-08-07 22:40:35 -0500

    nothing to see here

--- assets/Resources/ui-window-closeonback.js
+++ assets/Resources/ui-window-closeonback.js
@@ -1,9 +1,9 @@
-var win = Titanium.UI.createWindow({backgroundColor: 'black'});
-var win2 = Ti.UI.createWindow({backgroundColor: 'blue'});
+var win = Titanium.UI.createWindow({backgroundColor: 'green'});
+var win2 = Ti.UI.createWindow({modal : true});
  
 var btn = Ti.UI.createButton({title: 'open win'});
 btn.addEventListener('click', function(){
-    win2.open({navBarHidden: true});
+    win2.open({animated : false});
     win2.addEventListener('click', function(){win2.close()});
 })
 win.add(btn);
